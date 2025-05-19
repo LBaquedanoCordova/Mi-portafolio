c6ce2b753eaa7cee500841450ccbb50822734f6d
Luis Baquedano <lfbcordova@gmail.com>
2025-05-19 13:52:18 -0500

    feat: initialize app entry point
    - Imports and initializes core modules
    - Serves as the main entry file linked to index.html

---
+++ js/app.js
@@ -0,0 +1,12 @@
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import { initNavbar } from "./modules/navbar.js";
+import { initTypingText } from "./modules/intro.js";
+import { initTech } from "./modules/tech.js";
+import { initCopyEmail } from "./modules/copyEmail.js";
+
+document.addEventListener('DOMContentLoaded', () => {
+    initNavbar();
+    initTypingText();
+    initTech();
+    initCopyEmail();
+});
