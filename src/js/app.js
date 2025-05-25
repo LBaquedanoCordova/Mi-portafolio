328cef75e95a5ca0465e243318984ecaa8a821f3
Luis Baquedano <lfbcordova@gmail.com>
2025-05-25 09:08:13 -0500

    chore: switch Sass compilation from CLI to Vite integration

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import '../css/style.css';
+import '../sass/main.scss';
 import { initNavbar } from "./modules/navbar.js";
 import { initTypingText } from "./modules/intro.js";
 import { initTech } from "./modules/tech.js";
