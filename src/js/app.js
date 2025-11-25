d8dca8d0093605e6d810306aea285b23f2fd9551
Luis Baquedano <lfbcordova@gmail.com>
2025-11-25 08:25:52 -0500

    feat(technologies-nav): add category filtering and update layout
    Implement new navigation bar for the Technologies section to enable category-based filtering.
    Update HTML structure, refine Sass mixins and variables, and adjust item rendering logic to support
    dynamic tech lists and improved visual consistency across breakpoints.

--- src/js/app.js
+++ src/js/app.js
@@ -2,12 +2,13 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import '../sass/main.scss';
 import { initNavbar } from "./modules/navbar.js";
 import { initTypingText } from "./modules/intro.js";
-import { initTech } from "./modules/tech.js";
+import { initDefaultCategory, /* animationListTech  */} from "./modules/tech.js";
 import { initCopyEmail } from "./modules/copyEmail.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     initNavbar();
     initTypingText();
-    initTech();
+    initDefaultCategory();
     initCopyEmail();
+    // animationListTech();
 });
